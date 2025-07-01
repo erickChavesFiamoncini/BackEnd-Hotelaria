0fceeb49bac26bbe65414ce5443643af2bde13b7
erickChavesFiamoncini <erickechaves@gmail.com>
2025-07-01 16:56:09 -0300

    criação das models

--- hotelaria/admin.py
+++ hotelaria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from hotelaria.models import Funcionario
+from hotelaria.models import tipoFuncionario
+from hotelaria.models import Servico
+
+admin.site.register(tipoFuncionario)
+admin.site.register(Funcionario)
+admin.site.register(Servico)
+
