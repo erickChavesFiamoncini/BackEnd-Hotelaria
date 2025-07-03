7e45b695851cd75d7600c6f88453cc25b90fbd6e
gustavoalvhnr <gustavoalves.ho@gmail.com>
2025-07-03 17:44:30 -0300

    Feat-2: Criadas as tabelas hospede, reserva e pagamento

--- hotelaria/admin.py
+++ hotelaria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Hospede, reserva, pagamento
 
 # Register your models here.
+admin.site.register(Hospede)
+admin.site.register(reserva)
+admin.site.register(pagamento)
--- hotelaria/models.py
+++ hotelaria/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Hospede(models.Model):
+    cpf = models.BigIntegerField(default=1)
+    email = models.EmailField(max_length=150)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class reserva(models.Model):
+    data = models.DateField(default= '')
+    noites = models.IntegerField(default='')
+    hospede = models.ForeignKey(Hospede, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'Reserva de {self.hospede.nome} - {self.data} - {self.noites} noites'
+
+class pagamento(models.Model):
+    valor = models.DecimalField(max_digits=8, decimal_places=2, default='')
+    data = models.DateField(default='')
+    forma = models.CharField(max_length=20)
+    reserva = models.ForeignKey(reserva, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.valor} - {self.forma} - {self.data}'
+    
+
+
