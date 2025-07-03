af475847abb6a5aa8b0aa7ec65ec83fbadba13f2
bialunelli <bialunelli11@gmail.com>
2025-07-03 14:53:44 -0300

    Models quarto e tipoQuarto criadas

--- hotelaria/admin.py
+++ hotelaria/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from hotelaria.models import Funcionario
 from hotelaria.models import tipoFuncionario
 from hotelaria.models import Servico
+from hotelaria.models import tipoQuarto
+from hotelaria.models import Quarto
 
 admin.site.register(tipoFuncionario)
 admin.site.register(Funcionario)
 admin.site.register(Servico)
+admin.site.register(tipoQuarto)
+admin.site.register(Quarto)
+
 
