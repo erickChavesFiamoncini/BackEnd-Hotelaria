9b8baf51f90802746d07103b4e3ac83112aa6bf1
erickChavesFiamoncini <erickechaves@gmail.com>
2025-07-04 13:28:26 -0300

    models alteração

--- hotelaria/models.py
+++ hotelaria/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 
 class Hospede(models.Model):
-    cpf = models.BigIntegerField(default=1)
+    cpf = models.BigIntegerField(default=0)
     email = models.EmailField(max_length=150)
     nome = models.CharField(max_length=100)
 
